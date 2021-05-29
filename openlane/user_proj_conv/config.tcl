e71a1415ab1b085c1396e2f86f479d8ae15e7f07
Khemarat Boonyapaluk <korla.march@gmail.com>
2021-05-29 10:14:36 -0400

    Update config.tcl

--- openlane/user_proj_conv/config.tcl
+++ openlane/user_proj_conv/config.tcl
@@ -23,7 +23,7 @@ set ::env(VERILOG_FILES) "\
 
 set ::env(CLOCK_PORT) ""
 set ::env(CLOCK_NET) "convolve.clk"
-set ::env(CLOCK_PERIOD) "100"
+set ::env(CLOCK_PERIOD) "1"
 
 set ::env(FP_SIZING) absolute
 set ::env(DIE_AREA) "0 0 900 600"
@@ -35,7 +35,13 @@ set ::env(GND_NETS) [list {vssd1} {vssd2} {vssa1} {vssa2}]
 set ::env(FP_PIN_ORDER_CFG) $script_dir/pin_order.cfg
 
 set ::env(PL_BASIC_PLACEMENT) 1
-set ::env(PL_TARGET_DENSITY) 0.9
+set ::env(PL_TARGET_DENSITY) 0.5
 
 # If you're going to use multiple power domains, then keep this disabled.
 set ::env(RUN_CVC) 0
+
+# TODO: look for the clock tree option 
+# Some openSTA config to enable critical path
+# set utilization: start with 50%, max 60%
+# option to add some adder structure in openlane, like full-adder
+# but yosys default is the fastest
